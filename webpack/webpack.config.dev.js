05067440d8d99cc446e17d697e66c9ade40b24eb
arif <a.rifislamm.cse@gmail.com>
2023-07-25 18:52:28 +0600

    webpack scss moduler added

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -81,6 +81,27 @@ module.exports = {
             loader: "css-loader",
           }
         ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+            {
+                loader: 'style-loader',
+            },
+            {
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true,
+                },
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                },
+            },
+        ],
       }
     ]
   }
